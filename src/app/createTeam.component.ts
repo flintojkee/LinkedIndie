3964ee298034d224b1192956c79e62146076ae90
DenysVasylenko <32224385+flintojkee@users.noreply.github.com>
2018-04-25 05:48:58 +0300

    7

--- src/app/createTeam.component.ts
+++ src/app/createTeam.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ApiService} from "./api.service"
+import {ApiService} from './api.service';
 
 @Component({
   selector: 'createTeam',
@@ -23,8 +23,8 @@ import {ApiService} from "./api.service"
 })
 export class CreateTeamComponent {
   constructor(public apiService: ApiService){}
-  teamData = {}
-  createTeam(){
+  teamData:any;
+  createTeam() {
     this.apiService.createTeam(this.teamData);
   }
 }
